226b69eecf8b2e89e251efadea23d723c24ff6ee
Ömer Faruk KASARCIOĞLU <omr.frk.ksrcglu@gmail.com>
2024-02-20 15:15:40 +0300

    TailwindCSS install project.

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  mode:'jit',
+  purge:['./src/**/*.{js,jsx,ts,tsx}','./public/index.html'],
+  darkMode:false,
+  content: [],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
